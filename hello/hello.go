bcad4a730fc7b8d6d67314d01cce1099c03195dc
unknown <pospgv14@gmail.com>
2024-07-21 14:34:55 +0300

    mymodule: changes for v0.1.0

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,8 @@ package hello
 
 import "fmt"
 
-func Hello(name string) {
-	fmt.Sprintf("Hello, %v!", name)
+func Hello(name string) string {
+	res := fmt.Sprintf("Hello, %v!", name)
+
+	return res
 }
